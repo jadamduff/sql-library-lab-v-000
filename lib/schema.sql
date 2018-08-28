cf734ac9a06936995a3ee251fb8c837d62b0b9d9
Adam Duff <j.adam.duff@gmail.com>
2018-08-28 04:50:56 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -20,4 +20,19 @@ CREATE TABLE books (
   title TEXT,
   year TEXT,
   series_id INTEGER
-)
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER
+);
