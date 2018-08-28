14a4265f7825ee27ddc3b44a6b7ef1f108ac78fc
Adam Duff <j.adam.duff@gmail.com>
2018-08-28 05:17:23 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -17,31 +17,30 @@ INSERT INTO books (title, year, series_id) VALUES
   ("The Fellowship of the Ring", "1954", 2),
   ("The Two Towers", "1954", 2),
   ("Return of the King", "1955", 2);
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES
+  ("Harry Potter", "The boy who lived.", "Wizard", 1, 1),
+  ("Voldemort", "Kill Harry", "Zombie Wizard", 1, 1),
+  ("Professor Quirrell", "Two faced", "Wizard-ish", 1, 1),
+  ("Gilderoy Lockhart", "Nobody remembers me.", "Wizard", 1, 1),
+  ("Frodo Baggins", "Ring bearer.", "Hobbit", 2, 2),
+  ("Gandalf", "The fixer", "Wizard", 2, 2),
+  ("Sauron", "Lord of Mordor", "Maia", 2, 2),
+  ("Aragorn", "Ranger of the North", "Human", 2, 2);
 
-  INSERT INTO characters (name, motto, species, series_id, author_id) VALUES
-    ("Harry Potter", "The boy who lived.", "Wizard", 1, 1),
-    ("Voldemort", "Kill Harry", "Zombie Wizard", 1, 1),
-    ("Professor Quirrell", "Two faced", "Wizard-ish", 1, 1),
-    ("Gilderoy Lockhart", "Nobody remembers me.", "Wizard", 1, 1),
-    ("Frodo Baggins", "Ring bearer.", "Hobbit", 2, 2),
-    ("Gandalf", "The fixer", "Wizard", 2, 2),
-    ("Sauron", "Lord of Mordor", "Maia", 2, 2),
-    ("Aragorn", "Ranger of the North", "Human", 2, 2);
-
-  INSERT INTO character_books (character_id, book_id) VALUES
-    (1, 1),
-    (1, 2),
-    (1, 3),
-    (2, 1),
-    (2, 2),
-    (2, 3),
-    (3, 1),
-    (4, 2),
-    (5, 4),
-    (5, 5),
-    (5, 6),
-    (6, 4),
-    (6, 5),
-    (6, 6),
-    (7, 4)
-    (8, 5);
+INSERT INTO character_books (character_id, book_id) VALUES
+  (1, 1),
+  (1, 2),
+  (1, 3),
+  (2, 1),
+  (2, 2),
+  (2, 3),
+  (3, 1),
+  (4, 2),
+  (5, 4),
+  (5, 5),
+  (5, 6),
+  (6, 4),
+  (6, 5),
+  (6, 6),
+  (7, 4)
+  (8, 5);
