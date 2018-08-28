7c6e17c68ee5783edf0bb4e843815c1800469315
Adam Duff <j.adam.duff@gmail.com>
2018-08-28 04:54:33 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES
+  ("Harry Potter", 1, 1),
+  ("Lord of the Rings", 2, 2);
+
+INSERT INTO subgenres (name) VALUES
+  ("Magic"),
+  ("Fantasy");
