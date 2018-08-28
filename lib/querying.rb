9fc370aec2dce8c4df76d332096c65550ed9c596
Adam Duff <j.adam.duff@gmail.com>
2018-08-28 05:39:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM series
+  JOIN authors ON series.id = authors.series_id
+  JOIN subgenres ON series.id = subgenres.series_id;"
 end
 
 def select_series_title_with_most_human_characters
